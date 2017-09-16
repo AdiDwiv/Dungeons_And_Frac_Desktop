94b58dd38f2d5d35acb9130c3e8ebb500d48257e
Aditya Dwivedi <ad629@cornell.edu>
2017-09-16 12:03:27 -0400

    Added Grid controller

--- src/sample/GridController.java
+++ src/sample/GridController.java
@@ -1,9 +1,16 @@
 package sample;
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.*;
+import javafx.scene.image.*;
 import javafx.stage.Stage;
 
 public class GridController {
+    public static GridPane getGrid() {
+        GridPane gridPane = new GridPane();
+        Image image = new Image("./resource/media/GrassSample.jpg");
+        ImageView iv1 = new ImageView();
+        iv1.setImage(image);
+        gridPane.add(iv1, 0, 0, 1, 1);
+        return gridPane;
+    }
 }

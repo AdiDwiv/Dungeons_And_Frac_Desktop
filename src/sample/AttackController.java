1742c3e9a7bb576ae254250028f1e84059a873ff
cassandrascarpa <cls354@cornell.edu>
2017-09-16 19:02:30 -0400

    modified attack screen layout

--- src/sample/AttackController.java
+++ src/sample/AttackController.java
@@ -1,20 +1,41 @@
 package sample;
+import java.util.LinkedList;
+
 import javafx.application.Application;
 import javafx.scene.layout.*;
 import javafx.scene.text.Text;
 import javafx.scene.control.Button;
 import javafx.scene.image.*;
 import javafx.stage.Stage;
+import resource.Monster;
+import resource.Player;
 
 public class AttackController {
-	   public static BorderPane getPane() {
+	
+	LinkedList<String> expressions;
+	Image monsterImg, playerImg;
+	
+	public AttackController(Monster monster, Player player) {
+		expressions = monster.reducedExpressions;
+		monsterImg = new Image("./resource/media/"+monster.getImageCode());
+		playerImg = new Image("./resource/media/"+player.getImageCode());
+	}
+	
+	   public BorderPane getPane() {
 	        BorderPane attackPane = new BorderPane();
 	        //battle image
 	        attackPane.setStyle("-fx-background-image: url(\"./resource/media/GrassSample.jpg\");-fx-background-size: 500, 500;-fx-background-repeat: no-repeat;");
-	        ImageView playerImg = new ImageView();
-	        attackPane.setCenter(playerImg);
-	        ImageView monsterImg = new ImageView();
-	        attackPane.setCenter(playerImg);
+	        ImageView playerView = new ImageView();
+	        attackPane.setCenter(playerView);
+	        playerView.setFitHeight(25);
+	        playerView.setFitWidth(25);
+	        playerView.setImage(playerImg);
+	        attackPane.setCenter(playerView);
+	        ImageView monsterView = new ImageView();
+	        monsterView.setFitHeight(25);
+	        monsterView.setFitWidth(25);
+	        monsterView.setImage(monsterImg);
+	        attackPane.setCenter(monsterView);
 	        HBox hbox = new HBox();
 	        hbox.setStyle("-fx-background-color: white;");
 	        Text text = new Text();

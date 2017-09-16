ce71aa7c159c6e67e39f0f5985b91562d9ee13ff
aniroodh-ravikumar <ar765@cornell.edu>
2017-09-16 14:32:20 -0400

    Adding path finding function

--- src/resource/MapGrid.java
+++ src/resource/MapGrid.java
@@ -1,5 +1,7 @@
 package resource;
 
+import java.util.LinkedList;
+
 public class MapGrid {
     public CellState[][] map;
     CellState player;
@@ -10,4 +12,72 @@ public class MapGrid {
     }
 
 
+    public LinkedList<CellState> getPath(Location start, Location end) {
+    		LinkedList<CellState> path = new LinkedList<CellState>();
+    		int xDistance = end.x - start.x;
+    		int yDistance = end.y - start.y;
+    		Location currentLocation = start;
+    		while(currentLocation!=end) {
+    		State currentState = map[currentLocation.x][currentLocation.y].getState();
+    		
+    		while(xDistance!=0 && yDistance!=0) {
+    		
+    		if(xDistance>0) {
+    			
+    			for(int i = currentLocation.x + 1;currentState == State.VACANT && xDistance>0; i++) {
+    				path.add(map[i][currentLocation.y]);
+    				currentLocation.x++;
+    				currentState = map[currentLocation.x][currentLocation.y].getState();
+    			}
+    			
+    			if(yDistance>0) {
+    				for(int i = currentLocation.y + 1;currentState == State.VACANT && yDistance>0; i++) {
+        				path.add(map[currentLocation.x][i]);
+        				currentLocation.y++;
+        				currentState = map[currentLocation.x][currentLocation.y].getState();
+        			}
+    			}
+    			
+    			else {
+    				for(int i = currentLocation.y - 1;currentState == State.VACANT && yDistance<0; i--) {
+        				path.add(map[currentLocation.x][i]);
+        				currentLocation.y--;
+        				currentState = map[currentLocation.x][currentLocation.y].getState();
+        			}
+    			}
+    			
+    		}
+    		
+    		else {
+    			for(int i = currentLocation.x + 1;currentState == State.VACANT && xDistance>0; i++) {
+    				path.add(map[i][currentLocation.y]);
+    				currentLocation.x++;
+    				currentState = map[currentLocation.x][currentLocation.y].getState();
+    			}
+    			
+    			if(yDistance>0) {
+    				for(int i = currentLocation.y + 1;currentState == State.VACANT && yDistance>0; i++) {
+        				path.add(map[currentLocation.x][i]);
+        				currentLocation.y++;
+        				currentState = map[currentLocation.x][currentLocation.y].getState();
+        			}
+    			}
+    			
+    			else {
+    				for(int i = currentLocation.y - 1;currentState == State.VACANT && yDistance<0; i--) {
+        				path.add(map[currentLocation.x][i]);
+        				currentLocation.y--;
+        				currentState = map[currentLocation.x][currentLocation.y].getState();
+        			}
+    			}
+    		}
+    		
+    		}
+    		
+    		}
+    		
+    		return path;
+    		
+    }
+    
 }

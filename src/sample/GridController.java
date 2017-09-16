baacde8e72b47ccd994ea976ff03304286294022
Aditya Dwivedi <ad629@cornell.edu>
2017-09-16 11:52:18 -0400

    Added media folder

--- src/sample/GridController.java
+++ src/sample/GridController.java
@@ -1,4 +1,9 @@
 package sample;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 public class GridController {
 }

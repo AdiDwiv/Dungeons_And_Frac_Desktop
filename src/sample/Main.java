73eabf63e608726919b3fcb2ae3a6f1364818b82
aniroodh-ravikumar <ar765@cornell.edu>
2017-09-16 13:40:35 -0400

    Changed main title bar

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Dungeons and dragons");
         primaryStage.setScene(new Scene(GridController.getGrid(), 300, 275));
         primaryStage.show();
     }

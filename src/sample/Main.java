11827b28bc5ce36e3ca097222a6ca0cee4aa946b
aniroodh-ravikumar <ar765@cornell.edu>
2017-09-16 11:01:01 -0400

    Created launch screen class

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,8 +2,13 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -16,8 +21,7 @@ public class Main extends Application {
         primaryStage.show();
     }
 
-
     public static void main(String[] args) {
-        launch(args);
+    		launch(args);
     }
 }

3b496513297395c5f4e976ecdb668c8fab8b0589
aniroodh-ravikumar <ar765@cornell.edu>
2017-09-16 13:16:26 -0400

    Centered launch buttons and added logo placeholder

--- src/sample/LaunchScreen.java
+++ src/sample/LaunchScreen.java
@@ -1,5 +1,62 @@
 package sample;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+
+
+
 public class LaunchScreen {
-	
+	public static GridPane getLaunch() {
+        
+		GridPane logoGrid = new GridPane();
+		logoGrid.setAlignment(Pos.TOP_CENTER);
+		logoGrid.setHgap(10);
+        logoGrid.setVgap(10);
+        logoGrid.setPadding(new Insets(25, 25, 25, 25));
+		
+        GridPane buttonGrid = new GridPane();
+        buttonGrid.setAlignment(Pos.CENTER);
+        buttonGrid.setHgap(10);
+        buttonGrid.setVgap(10);
+        buttonGrid.setPadding(new Insets(25, 25, 25, 25));
+        buttonGrid.setStyle("-fx-background-color: #000000;");
+        
+        Image logo = new Image("./resource/media/GrassSample.jpg");
+        ImageView logoView = new ImageView();
+        logoView.setImage(logo);
+        logoView.setFitWidth(100);
+        logoView.setFitHeight(50);
+        buttonGrid.add(logoView, 1, 0);
+
+        Button newGame = new Button("New Game");
+        newGame.setAlignment(Pos.CENTER);
+        newGame.setStyle("-fx-background-color: #C0C0C0;");
+        buttonGrid.add(newGame, 1, 1);
+
+        Button loadGame = new Button("Load Game");
+        loadGame.setAlignment(Pos.CENTER);
+        loadGame.setStyle("-fx-background-color: #C0C0C0;");
+        buttonGrid.add(loadGame, 1, 2);
+        
+        Button howToPlay = new Button("How to Play");
+        howToPlay.setAlignment(Pos.CENTER);
+        howToPlay.setStyle("-fx-background-color: #C0C0C0;");
+        buttonGrid.add(howToPlay, 1, 3);
+        
+        newGame.setMinWidth(100);
+        loadGame.setMinWidth(100);
+        howToPlay.setMinWidth(100);
+        
+        return buttonGrid;
+	}
+
 }

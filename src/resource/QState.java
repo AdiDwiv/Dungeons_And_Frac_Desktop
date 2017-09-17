f648d086cbc4248787bcf5092984a0de04597978
cassandrascarpa <cls354@cornell.edu>
2017-09-16 23:37:05 -0400

    attack options

--- src/resource/QState.java
+++ src/resource/QState.java
@@ -3,7 +3,7 @@ package resource;
 public class QState {
 	private int correct;
 	private String nextReduced;
-	private String[][] options;
+	public String[][] options;
 	
 	public QState(int correct, String nextReduced, String[][] options) {
 		this.correct = correct;
